19d33ceba7bc728258541300418db5df4b882ce3
hvallee <hugo.v.vallee@gmail.com>
2016-01-15 12:30:38 +0100

    Improve Javadoc

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -7,26 +7,62 @@ package GUI;
  */
 public interface Controller {
 
-
+    /**
+     * Start the engine
+     */
     public void start();
 
+    /**
+     * Stop the engine
+     */
     public void stop();
 
+    /**
+     * Set the mark time boolean to true
+     */
     public void markTime();
 
+    /**
+     * Set the mark measure boolean to true
+     */
     public void markMeasure();
 
+    /**
+     * Inc the beat per measure
+     */
     public void inc();
 
+    /**
+     * Dec the beat per measure
+     */
     public void dec();
 
+    /**
+     * Set the tempo
+     * @param value
+     */
     public void userSlider(int value);
 
+    /**
+     *
+     * @return timeToMark
+     */
     public boolean getTimeToMark();
 
+
+    /**
+     *
+     * @return measureToMark
+     */
     public boolean getMeasureToMark();
 
+    /**
+     * Put timeToMark to false
+     */
     public void setTimeToMark();
 
+    /**
+     * Put measureToMark to false
+     */
     public void setMeasureToMark();
 }
--- src/GUI/ControllerImpl.java
+++ src/GUI/ControllerImpl.java
@@ -23,91 +23,55 @@ public class ControllerImpl implements Controller {
         measureToMark = false;
     }
 
-    /**
-     * Set the mark time boolean to true
-     */
     public void markTime() {
         timeToMark = true;
     }
 
-    /**
-     * Set the mark measure boolean to true
-     */
     public void markMeasure() {
         measureToMark = true;
     }
 
-    /**
-     * Inc the beat per measure
-     */
     @Override
     public void inc() {
         engine.setBeatPerMeasure(1);
     }
 
-    /**
-     * Dec the beat per measure
-     */
     @Override
     public void dec() {
         engine.setBeatPerMeasure(-1);
     }
 
-    /**
-     * Start the engine
-     */
     @Override
     public void start() {
         engine.setState(true);
     }
 
-    /**
-     * Stop the engine
-     */
     @Override
     public void stop() {
         engine.setState(false);
     }
 
-    /**
-     * Set the tempo
-     * @param value
-     */
     public void userSlider(int value) {
         if (engine != null) {
             engine.setTempo(Math.round(value));
         }
     }
 
-    /**
-     *
-     * @return timeToMark
-     */
     @Override
     public boolean getTimeToMark() {
         return timeToMark;
     }
 
-    /**
-     *
-     * @return measureToMark
-     */
     @Override
     public boolean getMeasureToMark() {
         return measureToMark;
     }
 
-    /**
-     * Put timeToMark to false
-     */
     @Override
     public void setTimeToMark() {
         timeToMark = false;
     }
 
-    /**
-     * Put measureToMark to false
-     */
     @Override
     public void setMeasureToMark() {
         measureToMark = false;
